346f1b307e95351f264686d9f0f3afb82c4c4863
khonsahumairo <khonsa.humairo@student.alhazen.academy>
2025-02-21 11:06:32 +0700

    Add files via upload

---
+++ ITv2.py
@@ -0,0 +1,14 @@
+nama_lengkap = input ("Masukkan nama lengkap")
+email = input ("Masukkan email")
+nomor_hp = input ("Masukkan nomor handphone")
+
+print("=" * 40)
+print("Formulir Pendaftaran Akun Siswa")
+print("=" * 40)
+print("\nNama lengkap :", nama_lengkap)
+print("Email          :", email)
+print("Nomor handphone :", nomor_hp)
+
+print("\nHi !", nama_lengkap)
+print("Selamat! Email", email, "berhasil terdaftar di sistem kami")
+print("\nLink untuk login ke aplikasi dikirim ke nomor", nomor_hp)
