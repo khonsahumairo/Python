9aa6407a9d6c1710f4bcedf747d8c4687285f0dd
khonsahumairo <khonsa.humairo@student.alhazen.academy>
2025-05-02 11:12:08 +0700

    Add files via upload

---
+++ data2.py
@@ -0,0 +1,33 @@
+range(1,10)
+print(list(range(1,10)))
+
+range(10)
+print(list(range(10)))
+
+range(1,10,2)
+print(list(range(1,10,2)))
+
+numbers = range(10)
+print(list(numbers))
+
+numbers = range(1,10,2)
+print(list(numbers))
+
+numbers = range(1,21)
+print(list(numbers))
+
+#positive indexing
+numbers[4]
+print(numbers[4])
+
+#negative indexing
+
+numbers[-3]
+print(numbers[-3])
+
+#slicing
+numbers = list(range(2,21,2))
+print(numbers[:5])
+print(numbers[2:6])
+print(numbers[2:8:3])
+
