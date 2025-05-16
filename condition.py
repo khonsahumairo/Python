cf89104b2849f176a7584a1937e8da453d65877f
khonsahumairo <khonsa.humairo@student.alhazen.academy>
2025-05-16 10:33:02 +0700

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,17 @@
+score = 75
+score = int(score)
+min_criteria = 75
+
+if score == min_criteria:
+    print("Anda berhasil naik level")
+
+score = 99
+min_criteria = 75
+
+if score >= min_criteria:
+    print("Anda berhasil naik level")
+elif score < min_criteria:
+    print("Anda belum berhasil naik level")
+else:
+    print("Nilai anda tidak ditemukan")
+
---
+++ loop.py
@@ -0,0 +1,12 @@
+#for
+for i in range(1, 10):
+    print(i)
+
+#while
+jumlah = input("input count = ")
+jumlah = int(jumlah)
+
+count = 1
+while count <= jumlah:
+    print("Ayo gess semangat ujian Qurannya!")
+    count += 1
