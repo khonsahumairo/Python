6926f923810618fc20fa8a92871150fd0b5145a0
khonsahumairo <khonsa.humairo@student.alhazen.academy>
2025-10-10 14:19:18 +0700

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+def triangle_area(base, height):
+    result = 1/2 * base * height
+    text = f"Area of the triangle is {result}"
+    print(text)
---
+++ main.py
@@ -0,0 +1,8 @@
+from mathematic.square.area import square_area
+from mathematic.triangle.area import triangle_area
+from mathematic.circle.area import circle_area
+
+square_area(15)
+triangle_area(10, 15)
+circle_area(12)
+
