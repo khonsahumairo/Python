40f8cf1d1c98316f5b300f313f1f997f6bd6f68a
khonsahumairo <khonsa.humairo@student.alhazen.academy>
2025-10-17 13:29:40 +0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,7 @@
+from stringcolor import *
+
+print(cs("Selamat Datang", "blue", "white"))
+print(bold("selamat datang"))
+print(underline("selamat datang"))
+print(underline("selamat datang").bold())
+print(cs("selamat datang", "blue","yellow").underline())
